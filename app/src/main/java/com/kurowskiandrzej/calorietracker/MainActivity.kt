89791a4b45efbb4513be1ff446285d68ce65f56a
Andrzej Kurowski <kurowskiandrzej@mail.com>
2022-05-02 14:27:03 +0200

    add AgeScreen.kt and AgeViewModel.kt

--- app/src/main/java/com/kurowskiandrzej/calorietracker/MainActivity.kt
+++ app/src/main/java/com/kurowskiandrzej/calorietracker/MainActivity.kt
@@ -3,12 +3,17 @@ package com.kurowskiandrzej.calorietracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.kurowskiandrzej.calorietracker.navigation.navigate
 import com.kurowskiandrzej.calorietracker.ui.theme.CalorieTrackerTheme
 import com.kurowskiandrzej.core.navigation.Route
+import com.kurowskiandrzej.onboarding_presentation.age.AgeScreen
 import com.kurowskiandrzej.onboarding_presentation.gender.GenderScreen
 import com.kurowskiandrzej.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,39 +25,48 @@ class MainActivity : ComponentActivity() {
         setContent {
             CalorieTrackerTheme {
                 val navController = rememberNavController()
-                NavHost(
-                    navController = navController,
-                    startDestination = Route.WELCOME
+                val scaffoldState = rememberScaffoldState()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState
                 ) {
-                    composable(Route.WELCOME) {
-                        WelcomeScreen(onNavigate = navController::navigate)
-                    }
-                    composable(Route.GENDER) {
-                        GenderScreen(onNavigate = navController::navigate)
-                    }
-                    composable(Route.AGE) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.WELCOME
+                    ) {
+                        composable(Route.WELCOME) {
+                            WelcomeScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.GENDER) {
+                            GenderScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.AGE) {
+                            AgeScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(Route.HEIGHT) {
 
-                    }
-                    composable(Route.HEIGHT) {
+                        }
+                        composable(Route.WEIGHT) {
 
-                    }
-                    composable(Route.WEIGHT) {
+                        }
+                        composable(Route.NUTRIENT_GOAL) {
 
-                    }
-                    composable(Route.NUTRIENT_GOAL) {
+                        }
+                        composable(Route.ACTIVITY) {
 
-                    }
-                    composable(Route.ACTIVITY) {
+                        }
+                        composable(Route.GOAL) {
 
-                    }
-                    composable(Route.GOAL) {
+                        }
+                        composable(Route.TRACKER_OVERVIEW) {
 
-                    }
-                    composable(Route.TRACKER_OVERVIEW) {
+                        }
+                        composable(Route.SEARCH) {
 
-                    }
-                    composable(Route.SEARCH) {
-
+                        }
                     }
                 }
             }
---
+++ onboarding/onboarding_presentation/src/main/java/com/kurowskiandrzej/onboarding_presentation/age/AgeViewModel.kt
@@ -0,0 +1,51 @@
+package com.kurowskiandrzej.onboarding_presentation.age
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kurowskiandrzej.core.domain.preferences.Preferences
+import com.kurowskiandrzej.core.domain.use_case.FilterOutDigits
+import com.kurowskiandrzej.core.util.UiEvent
+import com.kurowskiandrzej.core.util.UiText
+import com.kurowskiandrzej.core.R
+import com.kurowskiandrzej.core.navigation.Route
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AgeViewModel @Inject constructor(
+    private val preferences: Preferences,
+    private val filterOutDigits: FilterOutDigits
+) : ViewModel() {
+    var age by mutableStateOf("20")
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onAgeEnter(age: String) {
+        if (age.length <= 3) {
+            this.age = filterOutDigits(age)
+        }
+    }
+
+    fun onNextClick() {
+        viewModelScope.launch {
+            val ageNumber = age.toIntOrNull() ?: kotlin.run {
+                _uiEvent.send(
+                    UiEvent.ShowSnackbar(
+                        UiText.StringResource(R.string.error_age_cant_be_empty)
+                    )
+                )
+                return@launch
+            }
+            preferences.saveAge(ageNumber)
+            _uiEvent.send(UiEvent.Navigate(Route.HEIGHT))
+        }
+    }
+}
