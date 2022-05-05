c26884299564c6a5215612aa1c00c32cce2fa5c5
Andrzej Kurowski <kurowskiandrzej@mail.com>
2022-05-05 23:02:46 +0200

    add and display ExpandableMeal.kt

--- tracker/tracker_presentation/src/main/java/com/kurowskiandrzej/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/kurowskiandrzej/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -2,6 +2,7 @@ package com.kurowskiandrzej.tracker_presentation.tracker_overview
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -9,6 +10,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.kurowskiandrzej.core.util.UiEvent
 import com.kurowskiandrzej.core_ui.LocalSpacing
 import com.kurowskiandrzej.tracker_presentation.tracker_overview.components.DaySelector
+import com.kurowskiandrzej.tracker_presentation.tracker_overview.components.ExpandableMeal
 import com.kurowskiandrzej.tracker_presentation.tracker_overview.components.NutrientHeader
 
 @Composable
@@ -41,6 +43,16 @@ fun TrackerOverviewScreen(
                     .padding(horizontal = spacing.spaceMedium)
             )
             Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+        items(state.meals) { meal ->
+            ExpandableMeal(
+                meal = meal,
+                onToggleClick = { viewModel.onEvent(TrackerOverViewEvent.OnToggleMealClick(meal)) },
+                content = {
+
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
